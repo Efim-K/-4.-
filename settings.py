eda0a5bec0fce0bb989097639424abf50b201825
Efim Kashapov <bambalabes@gmail.com>
2024-06-16 20:41:18 +0500

    Добавлен путь до JSON-файл выбранными вакансиями

--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 DATA_PATH = ROOT_PATH.joinpath('data')
 FILE_PATH_JSON = DATA_PATH.joinpath('vacancies.json')
+FILE_PATH_SELECT = DATA_PATH.joinpath('vacancies_select.json')
