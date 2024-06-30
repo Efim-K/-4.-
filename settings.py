770e940debd799419c54e3243348ecfc5c1c262f
Efim Kashapov <bambalabes@gmail.com>
2024-06-30 18:32:59 +0500

    Удаление неиспользуемых данных

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 DATA_PATH = ROOT_PATH.joinpath('data')
 FILE_PATH_JSON = DATA_PATH.joinpath('vacancies.json')
-FILE_PATH_SELECT = DATA_PATH.joinpath('vacancies_select.json')
 HH_URL = 'https://api.hh.ru/vacancies'
 
 FORMAT = '%(asctime)s %(message)s'
